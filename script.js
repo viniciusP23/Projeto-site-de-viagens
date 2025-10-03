9c4aa3ca8184ea3e3ef86b23d03ac207ac3563e9
viniciusP23 <vinicius_pereira1@live.com>
2025-10-03 20:44:52 -0300

    menu mobile

--- script.js
+++ script.js
@@ -15,8 +15,13 @@ document.addEventListener("scroll", () => {
 let btnMenuMob = document.getElementById("btn-menu-mob")
 let line1 = document.querySelector(".line-menu-mob-1")
 let line2 = document.querySelector(".line-menu-mob-2")
+let menuMobile = document.getElementById("menu-mobile")
+let body = document.querySelector("body")
 
 btnMenuMob.addEventListener("click", () => {
     line1.classList.toggle("ativo1")
     line2.classList.toggle("ativo2")
+
+    menuMobile.classList.toggle("abrir")
+    body.classList.toggle("no-overflow")
 })
