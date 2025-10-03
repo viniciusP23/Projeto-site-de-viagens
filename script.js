a4f84eb09b34832fd845f863c4e1815744d09203
viniciusP23 <vinicius_pereira1@live.com>
2025-10-02 22:05:36 -0300

    adicionando botão do menu mobile

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// scroll do menu do reader
 let navBar = document.getElementById("header")
 
 document.addEventListener("scroll", () => {
@@ -7,4 +8,15 @@ document.addEventListener("scroll", () => {
     }else {
         navBar.classList.remove("rolar")
     }
+})
+
+
+// botão do menu mobile
+let btnMenuMob = document.getElementById("btn-menu-mob")
+let line1 = document.querySelector(".line-menu-mob-1")
+let line2 = document.querySelector(".line-menu-mob-2")
+
+btnMenuMob.addEventListener("click", () => {
+    line1.classList.toggle("ativo1")
+    line2.classList.toggle("ativo2")
 })
